44a96003bc1a7512e2510b2e85294aec710e620d
Albert Moravec <albert.moravec@gmail.com>
2021-11-06 01:04:04 +0100

    Add rollup-svelte

--- config/dev.exs
+++ config/dev.exs
@@ -41,8 +41,12 @@ config :cn23, Cn23Web.Endpoint,
   debug_errors: true,
   secret_key_base: "bt3hV+okqI6Ie4hpOKUVb3BDwbzGCQxFC62CqM3MAp0NF9X5fo0w+zuHWXQHNsjo",
   watchers: [
-    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    node: [
+      "node_modules/rollup/dist/bin/rollup",
+      "--config",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
---
+++ lib/cn23_web/helpers/apps.ex
@@ -0,0 +1,21 @@
+defmodule Cn23Web.Apps do
+  use Cn23Web.AppsMacro, manifest_path: "/static/apps/manifest.json", application: :cn23
+
+  def collect_scripts([]), do: []
+
+  def collect_scripts(wanted) when is_list(wanted) do
+    Enum.map(wanted, &app_script/1)
+  end
+
+  def collect_styles([]), do: []
+
+  def collect_styles(wanted) when is_list(wanted) do
+    Enum.map(wanted, &app_style/1)
+  end
+
+  def include(conn, apps) when is_list(apps) do
+    conn
+    |> Plug.Conn.assign(:additional_scripts, collect_scripts(apps))
+    |> Plug.Conn.assign(:additional_styles, collect_styles(apps))
+  end
+end
